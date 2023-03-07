c6e299c5c3c91cd45494b48280eed87c3661c9d6
vladislava-ar <vladislava.dnk@gmail.com>
2023-03-07 13:04:27 +0200

    add alt to imgs

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ showMessage ();
 function forward() {
     let image = document.getElementById('image');
     image.src = "./images/fennec.jpg"
+    image.alt = "Fennec"
 }
 
 function back() {
     let image = document.getElementById('image');
     image.src = "./images/quokka.jpg"
+    image.alt = "Quokka"
 }
